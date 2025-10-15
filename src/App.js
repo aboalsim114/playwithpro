802f0eb83c6e5cdd01b72d478d134218d1e46f59
aboalsim114 <aboalsim114@gmail.com>
2025-10-15 19:04:57 +0200

    refactor: remove unused imports for Navbar, Hero, and GamesSection from App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
-import Hero from './Composants/Hero/Hero';
-import Navbar from './Composants/Navbar/Navbar';
-import GamesSection from './Composants/GamesSection/GamesSection';
+
 import Home from './Pages/HomePage/Home';
 
 function App() {
